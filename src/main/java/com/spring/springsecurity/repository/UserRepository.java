0443bac8f93e565dd3ca3762143f96d0675140ac
NIKITA0414 <waghaskarnikita@gmail.com>
2024-10-22 12:11:30 +0530

    Integrated Spring Security with Maven: added dependencies, configured security settings, and set up basic authentication.

--- src/main/java/com/spring/springsecurity/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.springsecurity.repository;
-
-import com.spring.springsecurity.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
