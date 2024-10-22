e82f792f7e9793146040ddcb22e46f76567d9a4d
NIKITA0414 <waghaskarnikita@gmail.com>
2024-10-22 12:10:26 +0530

    Integrated Spring Security with Maven: added dependencies, configured security settings, and set up basic authentication.

---
+++ src/main/java/com/spring/springsecurity/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.spring.springsecurity;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}
